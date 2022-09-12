773ecf36ccbef41306c924ab0d732678b3e38d42
tobey <fafoworatobi25@gmail.com>
2022-09-12 11:38:12 +0100

    shipper table

---
+++ shipper.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Shipper (
+    ShipperID int,
+    ShipperName varchar(255),
+    phone varchar(255)
+);
+
+INSERT INTO Shipper
+    (ShipperID, ShipperName, phone)
+VALUES
+    (1, 'Speedy Express',   '(503) 555-9831'),
+    (2,	'United Package',   '(503) 555-3199'),
+    (3,	'Federal Shipping',	'(503) 555-9931');
